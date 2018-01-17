e350cb5d26d5f18869e8f224926d3f5c91c4b709
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-17 23:41:19 +0900

    first commit

---
+++ RN-test3/Home.js
@@ -0,0 +1,133 @@
+import React, { Component } from 'react';
+import { 
+AppRegistry, TextInput,StyleSheet, Text, View, TouchableHighlight, AlertIOS, TouchableOpacity, AsyncStorage, NavigatorIOS,
+  } from 'react-native';
+
+import { StackNavigator } from 'react-navigation';
+
+export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      comment: ''
+    }
+  }
+
+  changeTitle(title){
+    this.setState({title})
+  }
+
+  changeComment(comment){
+    this.setState({comment})
+  }
+
+  buttonPressed(){
+    const arrayData = [];
+    if(!this.state.comment && !this.state.title){
+      AlertIOS.alert('タイトルとコメントを入力して下さい')
+    } else if(!this.state.title && this.state.comment){
+      AlertIOS.alert('タイトルを入力して下さい')
+    } else if(this.state.title && !this.state.comment){
+      AlertIOS.alert('コメントを入力して下さい')
+    } else if(this.state.comment && this.state.title){
+      // AlertIOS.alert(this.state.title + ' + ' + this.state.comment)
+      const data = {
+        title: this.state.title,
+        comment: this.state.comment
+      }
+      arrayData.push(data);
+      AsyncStorage.getItem('database_form').then((value) => {
+        if(value !== null){
+          const d = JSON.parse(value);
+          d.push(data)
+          AsyncStorage.setItem('database_form',JSON.stringify(d)).then(() => {
+            this.props.navigator.push({
+              component: List,
+              title: 'List of Things'
+            })
+          })
+        }else {
+          AsyncStorage.setItem('database_form',JSON.stringify(arrayData)).then(() =>              {
+            this.props.navigator.push({
+              component: List,
+              title: 'List of Things'
+            })
+          })
+        }
+      })
+    } 
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+      <View>
+
+      <TextInput
+      style={styles.input}
+      placeholder="Title"
+      returnKeyType="done"
+      value={this.state.title}
+      onChangeText={(title) => this.changeTitle(title)}
+      />
+
+      <TextInput
+      multiline={true}
+      style={styles.input, styles.textArea}
+      placeholder="Comment"
+      returnKeyType="done"
+      value={this.state.comment}
+      onChangeText={(comment) => this.changeComment(comment)}
+      />
+
+      <TouchableOpacity
+      style={styles.button}
+      onPress={() =>
+       this.buttonPressed()
+      }>
+      <Text style={styles.textButton}>Send</Text>
+      </TouchableOpacity>
+
+      </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+    marginTop: 80,
+    paddingLeft: 15,
+    paddingRight: 15
+  },
+  button: {
+    backgroundColor: 'skyblue',
+    paddingTop: 15,
+    paddingBottom: 15
+  },
+  textButton: {
+    textAlign: 'center',
+    color: 'white'
+  },
+  title: {
+    textAlign: 'center',
+    fontSize: 18,
+    marginBottom: 5,
+    marginTop: 10
+  },
+  input: {
+    height: 40,
+    borderColor: '#ccc',
+    borderWidth: 2,
+    marginBottom: 20
+  },
+  textArea: {
+    height: 60
+  }
+});
+
+
