ee715a8572fcf3e367b039cdfeda9c30bacd5b6a
book_mima <ken0930mata@ezweb.ne.jp>
2018-01-19 17:46:07 +0900

    first commit

--- chat_test2/App.js
+++ chat_test2/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { 
-  StyleSheet, Text, View, Platform, TextInput, CheckBox, TouchableOpacity, Button
+  StyleSheet, Text, View, Platform, TextInput, CheckBox, TouchableOpacity, Button 
 } from 'react-native';
 import {StackNavigator,} from 'react-navigation';
 import Login from './src/Login'
 import Sign_Up from './src/Sign_Up'
+import Chat from './src/Chat'
 
 const Navigation = StackNavigator({
   Login:{
@@ -13,6 +14,9 @@ const Navigation = StackNavigator({
   Sign_Up:{
     screen:Sign_Up,
   },
+  Chat:{
+    screen:Chat
+  }
 })
 
 export default Navigation;
